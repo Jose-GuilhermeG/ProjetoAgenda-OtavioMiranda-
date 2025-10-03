981b56830326315a0e147e5943734550dbe01467
Guilherme <jg21504956@gmail.com>
2025-10-03 11:15:36 -0300

    Commit inicial

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Projeto de Agenda do curso de js e Ts do otavio miranda
+
+- estrutura do projeto modificada
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    mode : 'development',
+    entry : path.resolve(__dirname,'frontend','main.js'),
+    output : {
+        path : path.resolve(__dirname,'public','assets','js'),
+        filename : 'bundle.js',
+    },
+    module : {
+        rules : [
+            {
+                exclude : /node_modules/,
+                test : /\.js$/,
+                use : {
+                    loader : 'babel-loader',
+                    options : {
+                        presets : ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devtool : 'source-map'
+
+}
